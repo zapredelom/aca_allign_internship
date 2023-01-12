0fe3211c27399216a08bc3495ace85d5c5c48b59
Azat <azat.manukyan@mobeus.cam>
2023-01-12 23:02:37 +0400

    constexpr/move/lambda/callable_objects

--- Collection/Collection.h
+++ Collection/Collection.h
@@ -1,31 +1,70 @@
+#include <initializer_list>
+#include <algorithm>
 template <typename T>
-class Collection{
+class Array{
 public:
-    Collection();
-    Collection(const Collection&);
-    Collection(int preallocationSize);
+    Array();
+    explicit Array(int preallocationSize);
+    Array(const Array&);
+    Array(const Array&&);
+    Array(std::initializer_list<T>);
+    T& operator [](int index) {return _data[index];}
+    int size() { return _size;}
+    int capacity() { return _capacity;}
+    void push_back(T t);
 
-    [[nodiscard]] T& operator[] (int index);
-    [[nodiscard]] const T& operator[] (int index) const;
 
-    [[nodiscard]] int size() const;
-    [[nodiscard]] int capacity() const;
-
-    ~Collection();
+    ~Array() {delete [] _data;}
 private:
-    int _size;
     int _capacity;
+    int _size;
     T* _data;
-
-
 
 private: // magic numbers
     static const int kDefaultCapacity = 10;
-
 };
 
-template <typename T>
-Collection<T>::Collection():_capacity(kDefaultCapacity), _size(0), _data(new T[_capacity]) {
 
+template <typename T>
+Array<T>::Array(): Array(kDefaultCapacity) {}
+
+
+template <typename T>
+Array<T>:: Array(int size):
+    _capacity(size), _size(0), _data(new T[_capacity])
+{
 }
 
+template<typename T>
+Array<T> :: Array(const Array<T>& op2) : Array(op2._capacity)
+{
+    std::memcpy(op2._data, _data, sizeof(T) * op2._size);
+    _size = op2._size;
+
+}
+template<typename T> 
+Array<T>::Array(Array&& other){
+    _data = other._data;
+    _size = other._size;
+    _capacity = other._capacity;
+    other._data = nullptr;
+}
+
+template<typename T>
+Array<T>::Array(std::initializer_list<T> lst):
+    Array(lst.size()) {
+        for(const T& t: lst) {
+            _data[_size++] = t;
+        }
+    }
+template<typename T> 
+void Array<T>::push_back(T t) {
+    if( _size < _capacity) {
+        _data[_size++] = t;
+    } else {
+        T* new_data = new T[_capacity * 2];
+        std::memcopy(new_data, _data, sizeof(T)*_size);
+        delete [] _data;
+        _data = new_data;
+    }
+}
--- Collection/main.cpp
+++ Collection/main.cpp
@@ -1,5 +1,77 @@
-#include "Collection.h"
+// #include "Collection.h"
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <functional>
 
-void main() {
-    Collection<int>  a(12);
+bool lessInt(int a, int b) { return a < b; }
+
+bool greaterInt(int a, int b) { return a > b; }
+
+class GreateThanObject {
+ public:
+  bool operator()(int a, int b) { return a > b; }
+};
+
+class GreatherFromBase {
+ public:
+  GreatherFromBase(int baseValue): baseValue(baseValue){}
+  bool operator () (int from){return from > baseValue;}
+ private:
+  int baseValue;
+};
+
+
+typedef bool (*Compare)(int, int);
+typedef bool (*flt)(int);
+
+void Filter(std::vector<int>& v, std::function<bool(int)> fn) {
+    for(int i: v)
+    {
+        if(fn(i)){
+            std::cout<<i<<std::endl;
+        }
+    }
+}
+inline bool isOdd(int i) {
+    return i&1;
+}
+
+
+template <typename T>
+void F(T t){
+    T x =5;
+}
+std::vector<int> generate(int n) {
+    std::vector<int> ret(n);
+    for(int i = 0; i < n; i++) {
+        ret[i] = i;
+    }
+    return ret;
+}
+
+constexpr int factorial(int n) {
+    if( n == 1 ) return 1;
+    return n * factorial(n-1);
+}
+constexpr int fn () {
+    return 10;
+}
+int main() {
+  auto genVec = generate(100);
+   constexpr int val = 10;
+   constexpr int constInt = factorial(fn());
+
+  //constInt++;
+
+
+  auto flt_lambda = [](int a, int b ){ return a>b;};
+  std::vector<int> v{1, 2, 63, 2, 36, 7, 1, 4, 9, 10};
+
+  v.push_back(10);
+
+  std::for_each(begin(v) + 2, end(v) - 1, [](int i){ std::cout<<i<<std::endl;} );
+  auto it = begin(v);
+
 }
